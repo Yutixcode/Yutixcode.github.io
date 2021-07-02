dfc1d3c0bd549a68b14fb890d16717662ef794f2
Yutixcode <Yutixcode@gmail.com>
2021-07-03 02:54:28 +0700

    Create utils.jl

---
+++ utils.jl
@@ -0,0 +1,13 @@
+function hfun_blogcomments()
+    html_str = """
+    <script src="https://utteranc.es/client.js"
+        repo="Yutixcode/Yutixcode.github.io"
+        issue-term="url"
+        label="Comment"
+        theme="github-dark"
+        crossorigin="anonymous"
+        async>
+    </script>
+    """
+    return html_str
+end
